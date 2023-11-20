15ad4d9ba6e57be5599de3fe2fc8b397cfe81e3c
Agung Yuliyanto <agung.96tm@gmail.com>
2023-11-20 13:59:21 +0700

    wrong class

--- tests/commands/yoasu-project.test.js
+++ tests/commands/yoasu-project.test.js
@@ -1,6 +1,6 @@
-const { YoasuCommand } = require('../../src/commands/yoasu-project')
+const { YoasuProjectCommand } = require('../../src/commands/yoasu-project')
 
 test('yoasu success created as instance without error', () => {
-  const command = new YoasuCommand()
+  const command = new YoasuProjectCommand()
   expect(command.command()).toBe('project:yoasu')
 })
